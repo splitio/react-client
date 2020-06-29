09606c64c84bad3f9765f24a85ba58e0ccf522c8
Emiliano Sanchez <emiliano.sanchez@split.io>
2020-06-29 20:00:13 -0300

    updated types

--- types/SplitFactory.d.ts
+++ types/SplitFactory.d.ts
@@ -10,6 +10,7 @@ import { ISplitContextValues, ISplitFactoryProps } from './types';
 declare class SplitFactory extends React.Component<ISplitFactoryProps, ISplitContextValues> {
     static defaultProps: ISplitFactoryProps;
     readonly state: Readonly<ISplitContextValues>;
+    readonly isFactoryExternal: boolean;
     constructor(props: ISplitFactoryProps);
     subscribeToEvents(client: SplitIO.IClient, updateOnSdkUpdate?: boolean, updateOnSdkTimedout?: boolean, updateOnSdkReady?: boolean, updateOnSdkReadyFromCache?: boolean): void;
     componentWillUnmount(): void;
