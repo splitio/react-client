3d4689f01ecb295e9ad299bf51fa03843a47c269
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-02-03 20:50:29 -0300

    updated comments

--- src/SplitTreatments.tsx
+++ src/SplitTreatments.tsx
@@ -7,9 +7,6 @@ import { getControlTreatmentsWithConfig, WARN_ST_NO_CLIENT } from './constants';
  * SplitTreatments accepts a list of split names and optional attributes. It access the client at SplitContext to
  * call 'client.getTreatmentsWithConfig()' method, and passes the returned treatments to a child as a function.
  *
- * Since it is a PureComponent, it does a shallow comparison of props to determine if the component should update,
- * i.e., it uses reference identity for `names` and `attributes` props.
- *
  * @see {@link https://help.split.io/hc/en-us/articles/360020448791-JavaScript-SDK#get-treatments-with-configurations}
  */
 class SplitTreatments extends React.Component<ISplitTreatmentsProps> {
--- types/SplitTreatments.d.ts
+++ types/SplitTreatments.d.ts
@@ -4,14 +4,10 @@ import { ISplitTreatmentsProps } from './types';
  * SplitTreatments accepts a list of split names and optional attributes. It access the client at SplitContext to
  * call 'client.getTreatmentsWithConfig()' method, and passes the returned treatments to a child as a function.
  *
- * Since it is a PureComponent, it does a shallow comparison of props to determine if the component should update,
- * i.e., it uses reference identity for `names` and `attributes` props.
- *
  * @see {@link https://help.split.io/hc/en-us/articles/360020448791-JavaScript-SDK#get-treatments-with-configurations}
  */
 declare class SplitTreatments extends React.Component<ISplitTreatmentsProps> {
     logWarning?: boolean;
-    shouldComponentUpdate(nextProps: Readonly<ISplitTreatmentsProps>): boolean;
     render(): JSX.Element;
     componentDidMount(): void;
 }
