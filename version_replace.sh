a5622d6496dd39f2a031d725f47a4fd7df22eb95
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-02-09 19:55:00 -0300

    polishing

--- version_replace.sh
+++ version_replace.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# copy javascript-client builds locally
+cp -r ./node_modules/@splitsoftware/splitio/es  ./es/splitio
+cp -r ./node_modules/@splitsoftware/splitio/lib  ./lib/splitio
+
+# replace javascript-client imports to use local copy
+replace '@splitsoftware/splitio' './splitio' ./lib/index.js ./es/index.js ./lib/utils.js ./es/utils.js ./types/index.d.ts
+
+# replace javascript-client package.json files to use browser modules
+replace '"main": "./node.js"' '"main": "./browser.js"' ./lib/splitio ./es/splitio -r --include="package.json"
+
+# replace React SDK version placeholder
 VERSION=$(node -e "(function () { console.log(require('./package.json').version) })()")
 
 replace 'REACT_SDK_VERSION_NUMBER' $VERSION ./lib/constants.js ./es/constants.js
