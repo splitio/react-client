fb94754c56b86ed749a5808bdc2e19d863b3c7b7
Emiliano Sanchez <emiliano.sanchez@split.io>
2022-12-15 11:45:00 -0300

    refactor umd.ts file

--- umd.ts
+++ umd.ts
@@ -1,24 +1,10 @@
-// Split SDK factory (Renamed to avoid name conflict with SplitFactory component)
-import { SplitFactory as SplitSdk } from '@splitsoftware/splitio/client';
-
-// HOC functions
-import withSplitFactory from './src/withSplitFactory';
-import withSplitClient from './src/withSplitClient';
-import withSplitTreatments from './src/withSplitTreatments';
-
-// Render props components
-import SplitTreatments from './src/SplitTreatments';
-import SplitClient from './src/SplitClient';
-import SplitFactory from './src/SplitFactory';
-
-// helper functions/hooks
-import useClient from './src/useClient';
-import useTreatments from './src/useTreatments';
-import useTrack from './src/useTrack';
-import useManager from './src/useManager';
-
-// SplitContext
-import SplitContext from './src/SplitContext';
+import {
+  SplitSdk,
+  withSplitFactory, withSplitClient, withSplitTreatments,
+  SplitFactory, SplitClient, SplitTreatments,
+  useClient, useTreatments, useTrack, useManager,
+  SplitContext,
+} from './src/index';
 
 export default {
   SplitSdk,
