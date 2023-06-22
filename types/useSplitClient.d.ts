b5535b88ce5b7a3d1c25ca7c74aa2b29f595ceb2
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-06-22 10:22:43 -0300

    update pre-commit hook

--- types/useSplitClient.d.ts
+++ types/useSplitClient.d.ts
@@ -1,6 +1,6 @@
 import { ISplitContextValues } from './SplitContext';
 /**
- * 'useSplitClient' is a hook that returns an Split Context object with the client and status corresponding to the provided key and trafficType.
+ * 'useSplitClient' is a hook that returns an Split Context object with the client and its status corresponding to the provided key and trafficType.
  * It uses the 'useContext' hook to access the context, which is updated by SplitFactory and SplitClient components in the hierarchy of components.
  *
  * @return A Split Context object
