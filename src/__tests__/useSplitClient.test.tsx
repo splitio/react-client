07ccecac00f6579ac34b32b4da00fb9eaa50f52d
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-07-22 00:39:53 -0300

    fixed useSplitClient to support changes on update options

--- src/__tests__/useSplitClient.test.tsx
+++ src/__tests__/useSplitClient.test.tsx
@@ -15,7 +15,7 @@ import { useSplitClient } from '../useSplitClient';
 import { SplitClient } from '../SplitClient';
 import { SplitContext } from '../SplitContext';
 
-test('useSplitClient', () => {
+test('useSplitClient must update on SDK events', () => {
   const outerFactory = SplitSdk(sdkBrowser);
   const mainClient = outerFactory.client() as any;
   const user2Client = outerFactory.client('user_2') as any;
@@ -127,4 +127,41 @@ test('useSplitClient', () => {
   expect(countUseSplitClientUser2WithUpdate).toEqual(countSplitContext + 3);
 
   expect(countNestedComponent).toEqual(4);
+});
+
+test('useSplitClient must support changes in update props', () => {
+  const outerFactory = SplitSdk(sdkBrowser);
+  const mainClient = outerFactory.client() as any;
+
+  let rendersCount = 0;
+
+  function InnerComponent(updateOptions) {
+    useSplitClient(undefined, undefined, undefined, updateOptions);
+    rendersCount++;
+    return null;
+  }
+
+  function Component(updateOptions) {
+    return (
+      <SplitFactory factory={outerFactory} >
+        <InnerComponent {...updateOptions} />
+      </SplitFactory>
+    )
+  }
+
+  const wrapper = render(<Component />);
+
+  act(() => mainClient.__emitter__.emit(Event.SDK_READY)); // trigger re-render
+  act(() => mainClient.__emitter__.emit(Event.SDK_UPDATE)); // do not trigger re-render because updateOnSdkUpdate is false by default
+  expect(rendersCount).toBe(2);
+
+  wrapper.rerender(<Component updateOnSdkUpdate={true} />); // trigger re-render
+  act(() => mainClient.__emitter__.emit(Event.SDK_UPDATE)); // trigger re-render because updateOnSdkUpdate is true now
+
+  expect(rendersCount).toBe(4);
+
+  wrapper.rerender(<Component updateOnSdkUpdate={false} />); // trigger re-render
+  act(() => mainClient.__emitter__.emit(Event.SDK_UPDATE)); // do not trigger re-render because updateOnSdkUpdate is false now
+
+  expect(rendersCount).toBe(5);
 });
