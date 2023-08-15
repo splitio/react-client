6b37b376190c24ffd5bceb5f11e62e37b43120e4
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-08-15 16:12:06 -0300

    useTrack update

--- src/useTrack.ts
+++ src/useTrack.ts
@@ -11,6 +11,6 @@ const noOpFalse = () => false;
  * @see {@link https://help.split.io/hc/en-us/articles/360020448791-JavaScript-SDK#track}
  */
 export function useTrack(key?: SplitIO.SplitKey, trafficType?: string): SplitIO.IBrowserClient['track'] {
-  const client = useClient(key, trafficType);
+  const client = useClient(key, trafficType, undefined, { updateOnSdkReady: false, updateOnSdkReadyFromCache: false });
   return client ? client.track.bind(client) : noOpFalse;
 }
