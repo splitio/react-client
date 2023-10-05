5c5274b240983de0363080370f86765906cd6a30
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-10-05 16:43:08 -0300

    add comment about useTrack implementation

--- src/useTrack.ts
+++ src/useTrack.ts
@@ -11,6 +11,7 @@ const noOpFalse = () => false;
  * @see {@link https://help.split.io/hc/en-us/articles/360020448791-JavaScript-SDK#track}
  */
 export function useTrack(key?: SplitIO.SplitKey, trafficType?: string): SplitIO.IBrowserClient['track'] {
+  // All update options are false to avoid re-renders. The track method doesn't need the client to be operational.
   const client = useClient(key, trafficType, undefined, { updateOnSdkReady: false, updateOnSdkReadyFromCache: false });
   return client ? client.track.bind(client) : noOpFalse;
 }
