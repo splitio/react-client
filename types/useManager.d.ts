ecd1b0c824efd154c90923917da48a27724868d6
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-11-14 11:19:44 -0300

    Stop versioning 'types' folder. No need to track since it's auto-generated code

--- types/useManager.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 'useManager' is a hook that returns the Manager instance from the Split factory.
- * It uses the 'useContext' hook to access the factory at Split context, which is updated by
- * the SplitFactory component.
- *
- * @return A Split Manager instance, or null if used outside the scope of SplitFactory
- * @see {@link https://help.split.io/hc/en-us/articles/360020448791-JavaScript-SDK#manager}
- */
-export declare function useManager(): SplitIO.IManager | null;
