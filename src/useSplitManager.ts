c42fd8c21ebd3c198cfa8ddb3f6939035c744d8e
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-11-15 14:37:10 -0300

    Replace @return with @returns, which is the correct JSDoc tag

--- src/useSplitManager.ts
+++ src/useSplitManager.ts
@@ -6,7 +6,7 @@ import { ISplitContextValues } from './types';
  * 'useSplitManager' is a hook that returns an Split Context object with the Manager instance from the Split factory.
  * It uses the 'useContext' hook to access the factory at Split context, which is updated by the SplitFactory component.
  *
- * @return An object containing the Split context and the Split Manager instance, which is null if used outside the scope of SplitFactory
+ * @returns An object containing the Split context and the Split Manager instance, which is null if used outside the scope of SplitFactory
  *
  * @example
  * ```js
