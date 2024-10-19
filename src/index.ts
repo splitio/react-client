13fa499d6611dc6ad1d96aaf09b3e3cd1dcd328e
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-10-19 14:20:22 -0300

    Remove deprecated modules

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,13 @@
 // Split SDK factory (Renamed to avoid name conflict with SplitFactory component)
 export { SplitFactory as SplitSdk } from '@splitsoftware/splitio/client';
 
-// HOC functions
-export { withSplitFactory } from './withSplitFactory';
-export { withSplitClient } from './withSplitClient';
-export { withSplitTreatments } from './withSplitTreatments';
-
 // Components
 export { SplitTreatments } from './SplitTreatments';
 export { SplitClient } from './SplitClient';
-export { SplitFactory } from './SplitFactory';
 export { SplitFactoryProvider } from './SplitFactoryProvider';
 
 // Hooks
-export { useClient } from './useClient';
-export { useTreatments } from './useTreatments';
 export { useTrack } from './useTrack';
-export { useManager } from './useManager';
 export { useSplitClient } from './useSplitClient';
 export { useSplitTreatments } from './useSplitTreatments';
 export { useSplitManager } from './useSplitManager';
