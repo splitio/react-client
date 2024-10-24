fec84b32dd89e9513e4b84c2d4a36ca34624c63a
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-10-24 17:51:31 -0300

    Polishing

--- src/SplitTreatments.tsx
+++ src/SplitTreatments.tsx
@@ -14,7 +14,7 @@ import { useSplitTreatments } from './useSplitTreatments';
 export function SplitTreatments(props: ISplitTreatmentsProps) {
   const { children } = props;
   // SplitTreatments doesn't update on SDK events, since it is inside SplitFactory and/or SplitClient.
-  const context = useSplitTreatments({ updateOnSdkReady: false, updateOnSdkReadyFromCache: false, ...props });
+  const context = useSplitTreatments({ ...props, updateOnSdkReady: false, updateOnSdkReadyFromCache: false });
 
   return (
     <SplitContext.Provider value={context}>
