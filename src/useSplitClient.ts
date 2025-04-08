02a26beea9484c29737b9482117f9eee2c93ca4d
Emiliano Sanchez <emiliano.sanchez@split.io>
2025-04-08 18:16:26 -0300

    Comment update

--- src/useSplitClient.ts
+++ src/useSplitClient.ts
@@ -31,7 +31,7 @@ export function useSplitClient(options?: IUseSplitClientOptions): ISplitContextV
   const context = useSplitContext();
   const { client: contextClient, factory } = context;
 
-  // @TODO Move `getSplitClient` side effects
+  // @TODO Move `getSplitClient` side effects and reduce the function cognitive complexity
   // @TODO Once `SplitClient` is removed, which updates the context, simplify next line as `const client = factory ? getSplitClient(factory, splitKey) : undefined;`
   const client = factory && splitKey ? getSplitClient(factory, splitKey) : contextClient;
 
